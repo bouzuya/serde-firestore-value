99983a480c62f2ea045fa050b1354841439467f9
bouzuya <m@bouzuya.net>
2023-09-20 10:04:04 +0900

    Extract error and serializer mod

---
+++ crates/serde-firestore-value/src/error.rs
@@ -0,0 +1,26 @@
+#[derive(Debug, thiserror::Error)]
+#[error(transparent)]
+pub struct Error {
+    #[from]
+    code: ErrorCode,
+}
+
+impl serde::ser::Error for Error {
+    fn custom<T: std::fmt::Display>(msg: T) -> Self {
+        Error {
+            code: ErrorCode::Custom(msg.to_string()),
+        }
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub(crate) enum ErrorCode {
+    #[error("{0}")]
+    Custom(String),
+    #[error("integer out of range")]
+    IntegerOutOfRange,
+    #[error("key must be a string")]
+    KeyMustBeAString,
+    #[error("maximum byte length (1,048,487 bytes = 1MiB - 89 bytes) exceeded")]
+    MaximumByteLengthExceeded,
+}

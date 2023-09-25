22afdb1857b36b9b9d877608265454cff0ccfcd9
bouzuya <m@bouzuya.net>
2023-09-25 20:45:21 +0900

    Refactor firestore_array_value_deserializer mod

--- crates/serde-firestore-value/src/de/firestore_array_value_deserializer.rs
+++ crates/serde-firestore-value/src/de/firestore_array_value_deserializer.rs
@@ -1,18 +1,20 @@
-use google::firestore::v1::{ArrayValue, Value};
+use std::slice::Iter;
+
+use google::firestore::v1::Value;
 
 use crate::Error;
 
 use super::{value_ext::ValueExt, FirestoreValueDeserializer};
 
 pub(super) struct FirestoreArrayValueDeserializer<'de> {
-    index: usize,
-    value: &'de Value,
+    iter: Iter<'de, Value>,
 }
 
 impl<'de> FirestoreArrayValueDeserializer<'de> {
     pub(super) fn new(value: &'de Value) -> Result<Self, Error> {
-        value.as_array()?;
-        Ok(Self { index: 0, value })
+        Ok(Self {
+            iter: value.as_array()?.values.iter(),
+        })
     }
 }
 
@@ -23,14 +25,9 @@ impl<'de> serde::de::SeqAccess<'de> for FirestoreArrayValueDeserializer<'de> {
     where
         T: serde::de::DeserializeSeed<'de>,
     {
-        let ArrayValue { values } = self.value.as_array()?;
-        if self.index < values.len() {
-            let value = &values[self.index];
-            self.index += 1;
-            seed.deserialize(FirestoreValueDeserializer::new(value))
-                .map(Some)
-        } else {
-            Ok(None)
-        }
+        self.iter
+            .next()
+            .map(|value| seed.deserialize(FirestoreValueDeserializer::new(value)))
+            .transpose()
     }
 }

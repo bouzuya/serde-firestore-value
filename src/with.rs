ab487df8ee070fe0b49dd6e6c19feb3f4a2221d4
bouzuya <m@bouzuya.net>
2024-01-01 07:05:28 +0900

    Add with::option_chrono_date_time_as_timestamp

--- src/with.rs
+++ src/with.rs
@@ -74,6 +74,8 @@
 #[cfg(feature = "chrono")]
 pub mod chrono_date_time_as_timestamp;
 pub mod lat_lng;
+#[cfg(feature = "chrono")]
+pub mod option_chrono_date_time_as_timestamp;
 pub mod option_lat_lng;
 pub mod option_string_as_reference;
 pub mod option_timestamp;
---
+++ src/with/option_chrono_date_time_as_timestamp.rs
@@ -0,0 +1,115 @@
+//! (De)serialize `Option<chrono::DateTime<chrono::Utc>>` as `timestampValue`.
+
+use prost_types::Timestamp;
+
+/// Deserialize `Option<chrono::DateTime<chrono::Utc>>` from `timestampValue` or `nullValue
+///
+/// # Example
+///
+/// ```rust
+/// # fn main() -> anyhow::Result<()> {
+/// use google_api_proto::google::firestore::v1::{value::ValueType, Value};
+/// use prost_types::Timestamp;
+/// use serde_firestore_value::{from_value, with::option_chrono_date_time_as_timestamp};
+///
+/// #[derive(Debug, Eq, PartialEq, serde::Deserialize)]
+/// struct S(
+///     #[serde(deserialize_with = "option_chrono_date_time_as_timestamp::deserialize")]
+///     Option<chrono::DateTime<chrono::Utc>>,
+/// );
+///
+/// let o = S(Some(
+///     chrono::DateTime::<chrono::FixedOffset>::parse_from_rfc3339(
+///         "1970-01-01T00:00:01.000000002Z",
+///     )?
+///     .with_timezone(&chrono::Utc),
+/// ));
+/// let v = Value {
+///     value_type: Some(ValueType::TimestampValue(Timestamp {
+///         seconds: 1_i64,
+///         nanos: 2_i32,
+///     })),
+/// };
+/// let d = from_value::<'_, S>(&v)?;
+/// assert_eq!(d, o);
+///
+/// let o = S(None);
+/// let v = Value {
+///     value_type: Some(ValueType::NullValue(0)),
+/// };
+/// let d = from_value::<'_, S>(&v)?;
+/// assert_eq!(d, o);
+/// #     Ok(())
+/// # }
+/// ```
+pub fn deserialize<'de, D>(
+    deserializer: D,
+) -> Result<Option<chrono::DateTime<chrono::Utc>>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let option_timestamp = crate::with::option_timestamp::deserialize(deserializer)?;
+    option_timestamp
+        .map(|Timestamp { seconds, nanos }| {
+            Ok(chrono::DateTime::<chrono::Utc>::from_naive_utc_and_offset(
+                chrono::NaiveDateTime::from_timestamp_opt(seconds, nanos as u32)
+                    .expect("timestamp"),
+                chrono::Utc,
+            ))
+        })
+        .transpose()
+}
+
+/// Serialize `Option<chrono::DateTime<chrono::Utc>` as `referenceValue` or `nullValue`.
+///
+/// # Example
+///
+/// ```rust
+/// # fn main() -> anyhow::Result<()> {
+/// use google_api_proto::google::firestore::v1::{value::ValueType, Value};
+/// use prost_types::Timestamp;
+/// use serde_firestore_value::{to_value, with::option_chrono_date_time_as_timestamp};
+///
+/// #[derive(Debug, Eq, PartialEq, serde::Serialize)]
+/// struct S(
+///     #[serde(serialize_with = "option_chrono_date_time_as_timestamp::serialize")]
+///     Option<chrono::DateTime<chrono::Utc>>,
+/// );
+///
+/// let o = S(Some(
+///     chrono::DateTime::<chrono::FixedOffset>::parse_from_rfc3339(
+///         "1970-01-01T00:00:01.000000002Z",
+///     )?
+///     .with_timezone(&chrono::Utc),
+/// ));
+/// let v = Value {
+///     value_type: Some(ValueType::TimestampValue(Timestamp {
+///         seconds: 1_i64,
+///         nanos: 2_i32,
+///     })),
+/// };
+/// let s = to_value(&o)?;
+/// assert_eq!(s, v);
+///
+/// let o = S(None);
+/// let v = Value {
+///     value_type: Some(ValueType::NullValue(0)),
+/// };
+/// let s = to_value(&o)?;
+/// assert_eq!(s, v);
+/// #     Ok(())
+/// # }
+/// ```
+pub fn serialize<S>(
+    option_date_time: &Option<chrono::DateTime<chrono::Utc>>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    let option_timestamp = option_date_time.as_ref().map(|date_time| Timestamp {
+        seconds: date_time.timestamp(),
+        nanos: date_time.timestamp_subsec_nanos() as i32,
+    });
+    crate::with::option_timestamp::serialize(&option_timestamp, serializer)
+}

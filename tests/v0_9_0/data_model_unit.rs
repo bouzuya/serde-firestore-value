b48a6be32bc7da106dbefdb391214cf417893f21
bouzuya <m@bouzuya.net>
2024-08-26 22:22:42 +0900

    Add tests for data_model_unit

---
+++ tests/v0_9_0/data_model_unit.rs
@@ -0,0 +1,15 @@
+#[test]
+fn test() -> anyhow::Result<()> {
+    use googleapis_tonic_google_firestore_v1::google::firestore::v1::{value::ValueType, Value};
+    use serde_firestore_value::{from_value, to_value};
+
+    let o = ();
+    let v = Value {
+        value_type: Some(ValueType::NullValue(0)),
+    };
+    let s = to_value(&o)?;
+    let d = from_value::<'_, ()>(&s)?;
+    assert_eq!(s, v);
+    assert_eq!(d, o);
+    Ok(())
+}

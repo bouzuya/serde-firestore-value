ffbc147f5db77e27e595d6294ffc5d3c4ad511f6
bouzuya <m@bouzuya.net>
2024-08-26 21:38:04 +0900

    Add tests for data_model_u32

---
+++ tests/v0_9_0/data_model_u32.rs
@@ -0,0 +1,24 @@
+#[test]
+fn test() -> anyhow::Result<()> {
+    use googleapis_tonic_google_firestore_v1::google::firestore::v1::{value::ValueType, Value};
+    use serde_firestore_value::{from_value, to_value};
+
+    let o = u32::MAX;
+    let v = Value {
+        value_type: Some(ValueType::IntegerValue(i64::from(u32::MAX))),
+    };
+    let s = to_value(&o)?;
+    let d = from_value::<'_, u32>(&s)?;
+    assert_eq!(s, v);
+    assert_eq!(d, o);
+
+    let o = u32::MIN;
+    let v = Value {
+        value_type: Some(ValueType::IntegerValue(i64::from(u32::MIN))),
+    };
+    let s = to_value(&o)?;
+    let d = from_value::<'_, u32>(&s)?;
+    assert_eq!(s, v);
+    assert_eq!(d, o);
+    Ok(())
+}

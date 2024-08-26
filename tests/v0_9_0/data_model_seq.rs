d7322200f7e0edfe086ea9b80e3b9f9108a379a1
bouzuya <m@bouzuya.net>
2024-08-26 22:48:42 +0900

    Add tests for data_model_seq

---
+++ tests/v0_9_0/data_model_seq.rs
@@ -0,0 +1,24 @@
+#[test]
+fn test() -> anyhow::Result<()> {
+    use googleapis_tonic_google_firestore_v1::google::firestore::v1::{
+        value::ValueType, ArrayValue, Value,
+    };
+    use serde_firestore_value::{from_value, to_value};
+
+    let o = vec![1, 2, 3];
+    let v = Value {
+        value_type: Some(ValueType::ArrayValue(ArrayValue {
+            values: [1, 2, 3]
+                .into_iter()
+                .map(|it| Value {
+                    value_type: Some(ValueType::IntegerValue(i64::from(it))),
+                })
+                .collect::<Vec<Value>>(),
+        })),
+    };
+    let s = to_value(&o)?;
+    let d = from_value::<'_, Vec<i32>>(&s)?;
+    assert_eq!(s, v);
+    assert_eq!(d, o);
+    Ok(())
+}

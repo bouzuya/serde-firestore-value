c435903586a6c637eee82f5f9411d94e4966a118
bouzuya <m@bouzuya.net>
2024-08-26 21:34:49 +0900

    Add tests for data_model_bool

---
+++ tests/v0_9_0/data_model_bool.rs
@@ -0,0 +1,24 @@
+#[test]
+fn test() -> anyhow::Result<()> {
+    use googleapis_tonic_google_firestore_v1::google::firestore::v1::{value::ValueType, Value};
+    use serde_firestore_value::{from_value, to_value};
+
+    let o = false;
+    let v = Value {
+        value_type: Some(ValueType::BooleanValue(false)),
+    };
+    let s = to_value(&o)?;
+    let d = from_value::<'_, bool>(&s)?;
+    assert_eq!(s, v);
+    assert_eq!(d, o);
+
+    let o = true;
+    let v = Value {
+        value_type: Some(ValueType::BooleanValue(true)),
+    };
+    let s = to_value(&o)?;
+    let d = from_value::<'_, bool>(&s)?;
+    assert_eq!(s, v);
+    assert_eq!(d, o);
+    Ok(())
+}

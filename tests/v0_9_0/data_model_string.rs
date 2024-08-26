54b5870f69808858c0b6420a342ade58596b90cb
bouzuya <m@bouzuya.net>
2024-08-26 21:54:18 +0900

    Add tests for data_model_string

---
+++ tests/v0_9_0/data_model_string.rs
@@ -0,0 +1,31 @@
+#[test]
+fn test() -> anyhow::Result<()> {
+    use googleapis_tonic_google_firestore_v1::google::firestore::v1::{value::ValueType, Value};
+    use serde_firestore_value::{from_value, to_value};
+
+    let o = "abc".to_owned();
+    let v = Value {
+        value_type: Some(ValueType::StringValue(o.clone())),
+    };
+    let s = to_value(&o)?;
+    let d = from_value::<'_, String>(&s)?;
+    assert_eq!(s, v);
+    assert_eq!(d, o);
+
+    let o = "x".repeat(1_048_487);
+    let v = Value {
+        value_type: Some(ValueType::StringValue(o.clone())),
+    };
+    let s = to_value(&o)?;
+    let d = from_value::<'_, String>(&s)?;
+    assert_eq!(s, v);
+    assert_eq!(d, o);
+
+    let o = "x".repeat(1_048_487 + 1);
+    assert_eq!(
+        to_value(&o).unwrap_err().to_string(),
+        "maximum byte length (1,048,487 bytes = 1MiB - 89 bytes) exceeded"
+    );
+
+    Ok(())
+}

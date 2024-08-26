f5aae5592cefd4d025ca1e8386c479b1afc357c1
bouzuya <m@bouzuya.net>
2024-08-26 22:41:34 +0900

    Add tests for data_model_newtype_variant

---
+++ tests/v0_9_0/data_model_newtype_variant.rs
@@ -0,0 +1,31 @@
+#[test]
+fn test() -> anyhow::Result<()> {
+    use googleapis_tonic_google_firestore_v1::google::firestore::v1::{
+        value::ValueType, MapValue, Value,
+    };
+    use serde_firestore_value::{from_value, to_value};
+
+    #[derive(Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
+    enum E {
+        N(u8),
+    }
+
+    let o = E::N(u8::MAX);
+    let v = Value {
+        value_type: Some(ValueType::MapValue(MapValue {
+            fields: [(
+                "N".to_owned(),
+                Value {
+                    value_type: Some(ValueType::IntegerValue(i64::from(u8::MAX))),
+                },
+            )]
+            .into_iter()
+            .collect::<std::collections::HashMap<String, Value>>(),
+        })),
+    };
+    let s = to_value(&o)?;
+    let d = from_value::<'_, E>(&s)?;
+    assert_eq!(s, v);
+    assert_eq!(d, o);
+    Ok(())
+}

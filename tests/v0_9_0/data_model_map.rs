62b3d4fc46a08c7dd0baf0fdadc4b724d29af7cb
bouzuya <m@bouzuya.net>
2024-08-26 23:14:13 +0900

    Add tests for data_model_map

---
+++ tests/v0_9_0/data_model_map.rs
@@ -0,0 +1,31 @@
+#[test]
+fn test() -> anyhow::Result<()> {
+    use googleapis_tonic_google_firestore_v1::google::firestore::v1::{
+        value::ValueType, MapValue, Value,
+    };
+    use serde_firestore_value::{from_value, to_value};
+
+    let o = [("k1".to_owned(), 1_i64), ("k2".to_owned(), 2_i64)]
+        .into_iter()
+        .collect::<std::collections::BTreeMap<String, i64>>();
+    let v = Value {
+        value_type: Some(ValueType::MapValue(MapValue {
+            fields: [("k1".to_owned(), 1_i64), ("k2".to_owned(), 2_i64)]
+                .into_iter()
+                .map(|(k, v)| {
+                    (
+                        k,
+                        Value {
+                            value_type: Some(ValueType::IntegerValue(i64::from(v))),
+                        },
+                    )
+                })
+                .collect::<std::collections::HashMap<String, Value>>(),
+        })),
+    };
+    let s = to_value(&o)?;
+    let d = from_value::<'_, std::collections::BTreeMap<String, i64>>(&s)?;
+    assert_eq!(s, v);
+    assert_eq!(d, o);
+    Ok(())
+}

6373d5e616fba70f5a63cda70b316942a5d10e3c
bouzuya <m@bouzuya.net>
2024-08-27 20:42:07 +0900

    Add tests for with_string_as_reference

---
+++ tests/v0_9_0/with_string_as_reference_serialize_with.rs
@@ -0,0 +1,44 @@
+#[test]
+fn test() -> anyhow::Result<()> {
+    use googleapis_tonic_google_firestore_v1::google::firestore::v1::{
+        value::ValueType, MapValue, Value,
+    };
+    use serde_firestore_value::{to_value, with::string_as_reference};
+
+    #[derive(Debug, Eq, PartialEq, serde::Serialize)]
+    struct S {
+        #[serde(serialize_with = "string_as_reference::serialize")]
+        r: String,
+        s: String,
+    }
+
+    let o = S {
+        r: "projects/p/databases/d/documents/c/1".to_string(),
+        s: "s1".to_string(),
+    };
+    let v = Value {
+        value_type: Some(ValueType::MapValue(MapValue {
+            fields: {
+                let mut fields = std::collections::HashMap::new();
+                fields.insert(
+                    "r".to_string(),
+                    Value {
+                        value_type: Some(ValueType::ReferenceValue(
+                            "projects/p/databases/d/documents/c/1".to_string(),
+                        )),
+                    },
+                );
+                fields.insert(
+                    "s".to_string(),
+                    Value {
+                        value_type: Some(ValueType::StringValue("s1".to_string())),
+                    },
+                );
+                fields
+            },
+        })),
+    };
+    let s = to_value(&o)?;
+    assert_eq!(s, v);
+    Ok(())
+}
